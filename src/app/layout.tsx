0ac22fde05b4e6c1c9b3979673c370aac711ed58
Justin <ifanultra@gmail.com>
2026-05-15 11:38:42 +0800

    chore: add Microsoft Clarity tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         gtag('config', 'AW-18159357442');
                     `
                 }} />
+                
+                {/* Microsoft Clarity */}
+                <Script id="microsoft-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(c,l,a,r,i,t,y){
+                            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "wrauprsmdk");
+                    `
+                }} />
             </head>
             <body className="font-sans antialiased bg-slate-50 text-slate-900 dark:bg-slate-950 dark:text-slate-50 min-h-screen flex flex-col">
                 <noscript>
