ee01b812541a2bbfbc521a41ca4b2f1dde2d3ba4
Justin <ifanultra@gmail.com>
2026-06-04 11:53:21 +0800

    fix: remove Order Uploader tool and exclude sanity/scripts from tsconfig

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,13 +2,10 @@ import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { zhHansLocale } from '@sanity/locale-zh-hans'
-import { lazy } from 'react'
 import { brandType } from './sanity/schemaTypes/brandType'
 import { categoryType } from './sanity/schemaTypes/categoryType'
 import { productType } from './sanity/schemaTypes/productType'
 import { articleType } from './sanity/schemaTypes/articleType'
-
-const OrderUploader = lazy(() => import('./sanity/plugins/orderUploader/index'));
 
 export default defineConfig({
   name: 'default',
@@ -18,15 +15,6 @@ export default defineConfig({
   dataset: 'production',
 
   plugins: [structureTool(), visionTool(), zhHansLocale()],
-  tools: (prev) => {
-    return prev.concat([
-      {
-        name: 'Order Uploader',
-        title: 'Order Uploader',
-        component: OrderUploader,
-      }
-    ])
-  },
 
   schema: {
     types: [brandType, categoryType, productType, articleType],
